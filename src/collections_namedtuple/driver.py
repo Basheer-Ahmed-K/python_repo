d860aabccbb219c870e5e114995f7cd0eb4b6f6e
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-14 08:46:04 +0530

    Unit test for Collections.namedtuple added

--- src/collections_namedtuple/driver.py
+++ src/collections_namedtuple/driver.py
@@ -1,7 +1,3 @@
-from collections import namedtuple
-
 from src.collections_namedtuple.util import printAverage
 
-total_students = int(input())
-table = namedtuple('studentInfo', input().split())
-printAverage(total_students, table)
+printAverage()
--- src/collections_namedtuple/util.py
+++ src/collections_namedtuple/util.py
@@ -1,8 +1,11 @@
 import logging
+from collections import namedtuple
 
 
-def printAverage(total_students, table):
+def printAverage():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
+    total_students = int(input())
+    table = namedtuple('studentInfo', input().split())
     marks = [table._make(input().split()).MARKS for i in range(total_students)]
     logging.info(sum(map(int, marks)) / total_students)
     return sum(map(int, marks)) / total_students
