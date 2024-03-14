310983b434f5c8fb13221a5a2dd74a00fca91629
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-14 16:29:58 +0530

    Unit test Updated for Min Max

--- test/test_min_max/test_min_max.py
+++ test/test_min_max/test_min_max.py
@@ -11,3 +11,18 @@ class TestMinMax(unittest.TestCase):
 3 7
 1 3
 4 0'''
+
+    def test_two(self):
+        self.assertEquals(MinMax(), 7)
+    '''4 2
+2 5
+8 7
+1 4
+4 0'''
+
+    def test_three(self):
+        self.assertEquals(MinMax(), 2)
+    '''3 2
+2 5
+1 4
+4 0'''
