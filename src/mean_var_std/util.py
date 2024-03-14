46e55f56d25d836435a183aa6bd7116fa04cd01f
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-14 17:55:53 +0530

    Mean_Var_Str file got updated

--- src/mean_var_std/util.py
+++ src/mean_var_std/util.py
@@ -1,8 +1,16 @@
 import numpy as np
+import logging
 
 
 def MeanVarStd():
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
     N, _ = map(int, input().split())
-    x = np.array([input().split() for _ in range(N)], int)
-    print(np.mean(x, axis=1), np.var(x, axis=0), round(np.std(x), 11), sep='\n')
-    return np.mean(x, axis=1), np.var(x, axis=0), round(np.std(x), 11)
+    array = np.array([input().split() for _ in range(N)], int)
+    row_means = np.mean(array, axis=1)
+    column_variances = np.var(array, axis=0)
+    array_std = np.std(array)
+    row_means_str = np.array2string(row_means, separator=' ')
+    column_variances_str = np.array2string(column_variances, separator=' ')
+    array_std_str = str(round(array_std, 11))
+    logging.info(row_means_str + " " + column_variances_str + " " + array_std_str)
+    return row_means_str + " " + column_variances_str + " " + array_std_str
