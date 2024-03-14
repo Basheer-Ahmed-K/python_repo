a232261011f48d1b8223247b28e454a5be4be638
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-14 06:04:56 +0530

    Text Alignment Unit Test added

---
+++ test/test_text_alignment/test_text_alignment.py
@@ -0,0 +1,33 @@
+import unittest
+from src.text_alignment.util import textAlignment
+
+
+class TextAlignment(unittest.TestCase):
+    def test_one(self):
+        self.maxDiff = None
+        self.assertEquals(textAlignment(5), '''    H    
+   HHH   
+  HHHHH  
+ HHHHHHH 
+HHHHHHHHH
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+                    HHHHHHHHH 
+                     HHHHHHH  
+                      HHHHH   
+                       HHH    
+                        H     
+''')
