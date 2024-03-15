0a7c3a88d18af790a36afb89a84cc7827b83bbab
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-15 10:19:09 +0530

    Test Cases for Iterables and Iterators added

---
+++ test/test_iterables_iterators/test_iterables_iterators.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.iterables_iterators.util import IterablesIterators
+
+
+class TestIterablesIterators(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(IterablesIterators(),0.833333)
+        '''4
+a a c d
+2'''
