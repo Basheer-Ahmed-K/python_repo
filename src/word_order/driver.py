96be493559ab22d6d7b64b601aa937579f18cd5e
Basheer-Ahmed-K <basheerahmed.k@diggibyte.com>
2024-03-15 09:47:42 +0530

    Word Order Solution Added

---
+++ src/word_order/driver.py
@@ -0,0 +1,3 @@
+from src.word_order.util import wordOrder
+
+wordOrder()
---
+++ src/word_order/util.py
@@ -0,0 +1,17 @@
+from collections import Counter
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def wordOrder():
+    ans = []
+    n = int(input())
+    ls = []
+    for _ in range(n):
+        ls.append(input())
+    dic = Counter(ls)
+    ans.append(str(len(dic.keys())))
+    ans.append(list(dic.values()))
+    logging.info(ans)
+    return str(ans)
